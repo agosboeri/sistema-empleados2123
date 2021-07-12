82b3d3768e76571133e26b56476d281428cae9e4
agosboeri <agos.boeri@gmail.com>
2021-07-11 22:07:45 -0300

    creacion app e index

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-try:
+from flask import Flask
+from flask import render_template
+from flaskext.mysql import MySQL
 
-    print(1)
+app = Flask(__name__) #instanciamos flask
 
-except:
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']=''
+app.config['MYSQL_DATABASE_DB']='sistema'
+mysql.init_app(app)
 
-    print(2)
 
-finally:
+@app.route('/') #rootear para decirle que cuando alguien entre a esta direccion (o sea mi servidor), redireccionar
+def index():
+    sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'laura', 'obamacapo@ciudad.com.ar', 'fotodelauraobama.jpg');" 
+    conn=mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
 
-    print(3)
+    return render_template('empleados/index.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
+
